285a3d82ed96a6d7606090c1d21719aeba69e81f
Atul310 <atuldhital2310@gmail.com>
2022-12-10 16:41:18 +0545

    commit

--- Week 1/Assignments/main.py
+++ Week 1/Assignments/main.py
@@ -1,1 +1,27 @@
-print('Hello world')
+import random
+option = ("rock", "paper", "scissors")# tupples for our choices 
+running=0
+while (running<5): # while loop to run 5 times 
+   
+    running+=1
+  
+    player = None #storing null value 
+    computer = random.choice(option)
+
+    while player not in option: # while our player variable doesnot choose tupples options
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+print("Thanks for playing!")
