8ea4d84b5348bee77abc03533265e2d652291581
Atul310 <atuldhital2310@gmail.com>
2022-12-18 10:03:57 +0545

    done

--- Week 2/Assignments/filehandling.py
+++ Week 2/Assignments/filehandling.py
@@ -5,8 +5,10 @@ The function should read the entire contents of the file and return the contents
 '''
 
 def read_file(filename):
+    with open ("Week 2\Assignments\my_file.txt","r") as filename:
+      return filename.read()
     # TODO: Open the file in read-only mode and read its contents. Return the contents of the file.
-    pass
+   
 
-contents = read_file("my_file.txt")
+contents = read_file(filename="my_file.txt")
 print(contents)  # Output: "Hello, world!\nI just did my first Assignment of week 2."
